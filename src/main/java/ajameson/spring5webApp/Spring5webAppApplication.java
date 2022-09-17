834174bda7676d58e8835ec38cc181b68675983f
jamesonarnett <jamesonarnett@yahoo.com>
2022-09-17 12:45:13 -0500

    Initial setup,Book&Author classes

---
+++ src/main/java/ajameson/spring5webApp/Spring5webAppApplication.java
@@ -0,0 +1,13 @@
+package ajameson.spring5webApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5webAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5webAppApplication.class, args);
+	}
+
+}
